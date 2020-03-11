05cfc95c43c97967e7b7d659c89a1f4c9a8bdfac
Mohammad hassan <mohammad.h.karem@gmail.con>
2020-03-11 18:07:15 +0300

    Setup to heroku

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ const geocode = require('./utils/geocode')
 const forecast = require('./utils/forecast')
 
 const app = express()
-
+const port = process.env.PORT || 300
 // define path for express config
 const publicDirectoryPath = path.join(__dirname, '../public')
 const viewsPath = path.join(__dirname, '../templates/views')
@@ -79,6 +79,6 @@ app.get('*', (req, res) => {
     })
 })
 
-app.listen(3000, () => {
-    console.log('server is work perfictly')
+app.listen(port, () => {
+    console.log('server is work perfictly on port' + port)
 })
